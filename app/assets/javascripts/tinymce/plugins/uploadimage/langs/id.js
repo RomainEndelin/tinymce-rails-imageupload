51bfab9c72bbd293a61151213f5830d5155ca838
Romain Endelin <rendelin@zendesk.com>
2017-04-19 23:14:24 +0200

    Add indonesian translation

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/id.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('id', {
+  'Insert an image from your computer': 'Masukan gambar dari komputer',
+  'Insert image': "Masukan gambar",
+  'Choose an image': "Pilih gambar",
+  'You must choose a file': "Harus masukan gambar",
+  'Got a bad response from the server': "Masalah teknis",
+  "Didn't get a response from the server": "Masalah teknis",
+  'Insert': "Masukan",
+  'Cancel': "Batalkan",
+  'Image description': "Deskripsi gambar",
+});
